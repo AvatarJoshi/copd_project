b755192f712b3327564390f2c9df2ae2b4b21880
Poonsri Singivipulya <puipui@Poonsris-MacBook-Pro.local>
2022-11-06 20:50:36 -0500

    Moved to previous commit with Avatar. Updated .gitignore. This should have all updates to map.

--- COPD_Mapping/static/js/logic4.js
+++ COPD_Mapping/static/js/logic4.js
@@ -1,214 +1,92 @@
+console.log("Starting ...");
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
 // Creating map object
 var myMap = L.map("map", {
   center: [38.50445, -98.39784],
   zoom: 5
 });
 
-
-// var map = L.map('map').setView([37.8, -96], 4);
-
-// var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
-
-// L.geoJson(statesData).addTo(map);
-
-// // Function to determine marker size based on population
-// function markerSize(population) {
-//   return population / 40;
-// }
-
-// // An array containing all of the information needed to create city and state markers
-// var locations = [
-//   {
-//     coordinates: [40.7128, -74.0059],
-//     state: {
-//       name: "New York State",
-//       population: 19795791
-//     },
-//     city: {
-//       name: "New York",
-//       population: 8550405
-//     }
-//   },
-//   {
-//     coordinates: [34.0522, -118.2437],
-//     state: {
-//       name: "California",
-//       population: 39250017
-//     },
-//     city: {
-//       name: "Lost Angeles",
-//       population: 3971883
-//     }
-//   },
-//   {
-//     coordinates: [41.8781, -87.6298],
-//     state: {
-//       name: "Illinois",
-//       population: 12671821
-//     },
-//     city: {
-//       name: "Chicago",
-//       population: 2695598
-//     }
-//   },
-//   {
-//     coordinates: [29.7604, -95.3698],
-//     state: {
-//       name: "Texas",
-//       population: 26960000
-//     },
-//     city: {
-//       name: "Houston",
-//       population: 2296224
-//     }
-//   },
-//   {
-//     coordinates: [41.2524, -95.9980],
-//     state: {
-//       name: "Nebraska",
-//       population: 1882000
-//     },
-//     city: {
-//       name: "Omaha",
-//       population: 446599
-//     }
-//   }
-// ];
-
-// // Define arrays to hold created city and state markers
-// var cityMarkers = [];
-// var stateMarkers = [];
-
-// // Loop through locations and create city and state markers
-// for (var i = 0; i < locations.length; i++) {
-//   // Setting the marker radius for the state by passing population into the markerSize function
-//   stateMarkers.push(
-//     L.circle(locations[i].coordinates, {
-//       stroke: false,
-//       fillOpacity: 0.75,
-//       color: "white",
-//       fillColor: "white",
-//       radius: markerSize(locations[i].state.population)
-//     })
-//   );
-
-//   // Setting the marker radius for the city by passing population into the markerSize function
-//   cityMarkers.push(
-//     L.circle(locations[i].coordinates, {
-//       stroke: false,
-//       fillOpacity: 0.75,
-//       color: "#green",
-//       fillColor: "green",
-//       radius: markerSize(locations[i].city.population)
-//     })
-//   );
-// }
-
-
-
-
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-// If data.beta.nyc is down comment out this link
-var data_county = "static/data/testData.geo.json";
-
+let baseMaps = {
+  "Streets": streets
+};
 
 // // If data.beta.nyc is down comment out this link
-// var data_county = "static/data/county.geo.json";
+var data_county = "static/data/county.geojson";
+console.log("print data path here : ", data_county);
 // //var data_county = "county.json";
 
+// If data.beta.nyc is down comment out this link
+// var data_state = "static/data/testData.geo.json";
+//var data_county = "static/data/county.geo.json";
 
+let allCounty = new L.LayerGroup();
 
-
-function getColor(d) {
-  return d > 1000 ? '#800026' :
-         d > 500  ? '#BD0026' :
-         d > 200  ? '#E31A1C' :
-         d > 100  ? '#FC4E2A' :
-         d > 50   ? '#FD8D3C' :
-         d > 20   ? '#FEB24C' :
-         d > 10   ? '#FED976' :
-                    '#FFEDA0';
+let overlays = {
+  "County View": allCounty
 }
+
+L.control.layers(baseMaps, overlays).addTo(myMap);
+
+// // Adding tile layer
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(myMap);
 
 
 // Grabbing our GeoJSON data..
 d3.json(data_county, function(data) {
-  console.log(data);
+  console.log("inside d3 func: ", data);
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
     // Style each feature (in this case a neighborhood)
     style: function(feature) {
       return {
-        color: "grey",
+        color: "black",
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: getColor(feature.properties.CODP),
+        fillColor: getColor(feature.properties.NAME10),
         fillOpacity: 1,
         weight: 1.5
       };
     },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      alert(feature.properties.NAME10);
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.9
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.5
-          });
-        },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-        click: function(event) {
-          myMap.fitBounds(event.target.getBounds());
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.NAME10 + "</h1> <hr> <h2>" + feature.properties.CODP + "</h2>");
-
+    onEachFeature: function(features, layer){
+      console.log(features)
+      layer.bindPopup("<h3>" + features.features)
     }
-  }).addTo(myMap);
+  }).addTo(allCounty);
+
+  allCounty.addTo(myMap);
+
 });
 
+// function init() {
+// d3.json("static/data/county.geo.json").then((data) => {
+//   console.log("data: ", data)
+//   var var1 = data.type;
+// });
+// }
+
+// init();
 
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+
+
+
+
+
 
 // // If data.beta.nyc is down comment out this link
 // var data_county = "static/data/testData.geo.json";
-// //var data_county = "test.json";
-
-// If data.beta.nyc is down comment out this link
-var data_state = "static/data/state.geo.json";
-//var data_county = "county.json";
-
 
 
 
@@ -225,97 +103,89 @@ var data_state = "static/data/state.geo.json";
 
 
 // Grabbing our GeoJSON data..
-d3.json(data_state, function(data) {
-  console.log(data);
-  // Creating a geoJSON layer with the retrieved data
-  L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
-    style: function(feature) {
-      return {
-        color: "black",
-        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: getColor(feature.properties.NAME10),
-        fillOpacity: 1,
-        weight: 1.5
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // alert(feature.properties.NAME10);
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.9
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.5
-          });
-        },
-        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-        click: function(event) {
-          myMap.fitBounds(event.target.getBounds());
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + feature.properties.NAME10);
+// d3.json(data_county, function(data) {
+//   console.log(data);
+//   // Creating a geoJSON layer with the retrieved data
+//   L.geoJson(data, {
+//     // Style each feature (in this case a neighborhood)
+//     style: function(feature) {
+//       return {
+//         color: "black",
+//         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+//         fillColor: getColor(feature.properties.NAME10),
+//         fillOpacity: 1,
+//         weight: 1.5
+//       };
+//     },
+    // // Called on each feature
+    // onEachFeature: function(feature, layer) {
+    //   alert(feature.properties.CODP);
+    //   // Set mouse events to change map styling
+    //   layer.on({
+    //     // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+    //     mouseover: function(event) {
+    //       layer = event.target;
+    //       layer.setStyle({
+    //         fillOpacity: 0.9
+    //       });
+    //     },
+  //       // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+  //       mouseout: function(event) {
+  //         layer = event.target;
+  //         layer.setStyle({
+  //           fillOpacity: 0.5
+  //         });
+  //       },
+  //       // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+  //       click: function(event) {
+  //         myMap.fitBounds(event.target.getBounds());
+  //       }
+  //     });
+  //     // Giving each feature a pop-up with information pertinent to it
+  //     layer.bindPopup("<h1>" + feature.properties.NAME10 + "</h1> <hr> <h2>" + feature.properties.CODP + "</h2>");
 
-    }
-  }).addTo(myMap);
-});
-
-
-
-
-
+  // }).addTo(myMap);
+  
 
 
 
 
 // ------ Earthquake legend —————
-let legend = L.control({
+// let legend = L.control({
   // position: "bottomright"
 
-});
+// });
 
-// Then add all the details for the legend
-legend.onAdd = function() {
-  let div = L.DomUtil.create("div", "info legend");
+// // Then add all the details for the legend
+// legend.onAdd = function() {
+//   let div = L.DomUtil.create("div", "info legend");
 
-  const d = [0, 1, 2, 3, 4, 5];
-  const colors = [
-    "#800026",
-    "#BD0026",
-    "#E31A1C",
-    "#FC4E2A",
-    "#FD8D3C",
-    "#FEB24C",
-    "FED976",
-    "FED976",
+//   const d = [0, 1, 2, 3, 4, 5];
+//   const colors = [
+//     "#800026",
+//     "#BD0026",
+//     "#E31A1C",
+//     "#FC4E2A",
+//     "#FD8D3C",
+//     "#FEB24C",
+//     "FED976",
+//     "FED976",
 
-  ];
-
-
-// Looping through our intervals to generate a label with a colored square for each interval.
-  for (var i = 0; i < d.length; i++) {
-    console.log(colors[i]);
-    div.innerHTML +=
-      "<i style='background: " + colors[i] + "'></i> " +
-      d[i] + (d[i + 1] ? "&ndash;" + d[i + 1] + "<br>" : "+");
-    }
-    return div;
-  };
-
-  // Finally, we our legend to the map.
-  legend.addTo(map);
+//   ];
 
 
+// // Looping through our intervals to generate a label with a colored square for each interval.
+//   for (var i = 0; i < d.length; i++) {
+//     console.log(colors[i]);
+//     div.innerHTML +=
+//       "<i style='background: " + colors[i] + "'></i> " +
+//       d[i] + (d[i + 1] ? "&ndash;" + d[i + 1] + "<br>" : "+");
+//     }
+//     return div;
+//   };
+
+//   // Finally, we our legend to the map.
+//   legend.addTo(map);
 
 
 
@@ -324,26 +194,7 @@ legend.onAdd = function() {
 
 
 
-// // Use this link to get the geojson data.
-// // var link = "static/data/nyc.geojson";
 
-// // Function that will determine the color of a neighborhood based on the borough it belongs to
-// // function chooseColor(borough) {
-// //   switch (borough) {
-// //   case "Brooklyn":
-// //     return "yellow";
-// //   case "Bronx":
-// //     return "red";
-// //   case "Manhattan":
-// //     return "orange";
-// //   case "Queens":
-// //     return "green";
-// //   case "Staten Island":
-// //     return "purple";
-// //   default:
-// //     return "black";
-// //   }
-// // }
 
 // // // Grabbing our GeoJSON data..
 // // d3.json(link, function(data) {
--- COPD_Mapping/static/js/test1.js
+++ COPD_Mapping/static/js/test1.js
@@ -1,5 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
 
 // Creating map object
 var myMap = L.map("map", {
@@ -8,7 +6,7 @@ var myMap = L.map("map", {
   });
 
 // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var light1 =L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -18,37 +16,46 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 });
 
 
+
+
+// // Only one base layer can be shown at a time
+// var baseMaps = {
+//   Light1: light1,
+//   Light2: light2
+// };
+
+
 // If data.beta.nyc is down comment out this link
 var data_county = "static/data/testData.geo.json";
-var data_state = "static/data/state.geo.json";
+// var data_state = "static/data/state.geo.json";
 
 // 1. Add a 2nd layer group for the tectonic plate data.
-let data_county = new L.LayerGroup();
-let data_state = new L.LayerGroup();
+let data_county = new L.LayerGroup(data_county);
+// let data_state = new L.LayerGroup(data_state);
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
-let overlays = {
-  "State": data_state,
-  "County": data_county
+// var overlays = {
+//   "State": data_state,
+//   "County": data_county
   
-};
+// };
 
 // Define a map object
 var myMap = L.map("map", {
   center: [38.50445, -98.39784],
   zoom: 3
-  layer: [myMap, data_state, data_county]
+  // layers: [myMap]
 });
 
 // Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+// // Add the layer control to the map
+// L.control.layers(myMap, {
+//   collapsed: false
+// }).addTo(myMap);
 
 // Then we add a control to the map that will allow the user to change which
 // layers are visible.
-L.control.layers(baseMaps, overlays).addTo(myMap);
+L.control.layers(myMap).addTo(myMap);
 
 // ------ Earthquake Info —————
 // Grabbing our GeoJSON data..
@@ -59,9 +66,9 @@ d3.json(data_county, function(data) {
       // Style each feature (in this case a neighborhood)
       style: function(feature) {
         return {
-          color: "grey",
+          color: "black",
           // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-          fillColor: getColor(feature.properties.CODP),
+          fillColor: getColor(feature.properties.NAME10),
           fillOpacity: 1,
           weight: 1.5
         };
@@ -99,157 +106,64 @@ d3.json(data_county, function(data) {
 data_county.addTo(myMap);
 
 
-  // Here we create a legend control object.
-let legend = L.control({
-  position: "bottomright"
-});
+//   // Here we create a legend control object.
+// let legend = L.control({
+//   position: "bottomright"
+// });
 
-// Grabbing our GeoJSON data..
-d3.json(data_state, function(data) {
-    console.log(data);
-    // Creating a geoJSON layer with the retrieved data
-    L.geoJson(data, {
-      // Style each feature (in this case a neighborhood)
-      style: function(feature) {
-        return {
-          color: "black",
-          // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-          fillColor: getColor(feature.properties.NAME10),
-          fillOpacity: 1,
-          weight: 1.5
-        };
-      },
-      // Called on each feature
-      onEachFeature: function(feature, layer) {
-        // alert(feature.properties.NAME10);
-        // Set mouse events to change map styling
-        layer.on({
-          // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-          mouseover: function(event) {
-            layer = event.target;
-            layer.setStyle({
-              fillOpacity: 0.9
-            });
-          },
-          // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-          mouseout: function(event) {
-            layer = event.target;
-            layer.setStyle({
-              fillOpacity: 0.5
-            });
-          },
-          // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-          click: function(event) {
-            myMap.fitBounds(event.target.getBounds());
-          }
-        });
-        // Giving each feature a pop-up with information pertinent to it
-        layer.bindPopup("<h1>" + feature.properties.NAME10);
+// // Grabbing our GeoJSON data..
+// d3.json(data_state, function(data) {
+//     console.log(data);
+//     // Creating a geoJSON layer with the retrieved data
+//     L.geoJson(data, {
+//       // Style each feature (in this case a neighborhood)
+//       style: function(feature) {
+//         return {
+//           color: "black",
+//           // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+//           fillColor: getColor(feature.properties.NAME10),
+//           fillOpacity: 1,
+//           weight: 1.5
+//         };
+//       },
+//       // Called on each feature
+//       onEachFeature: function(feature, layer) {
+//         // alert(feature.properties.NAME10);
+//         // Set mouse events to change map styling
+//         layer.on({
+//           // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+//           mouseover: function(event) {
+//             layer = event.target;
+//             layer.setStyle({
+//               fillOpacity: 0.9
+//             });
+//           },
+//           // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+//           mouseout: function(event) {
+//             layer = event.target;
+//             layer.setStyle({
+//               fillOpacity: 0.5
+//             });
+//           },
+//           // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+//           click: function(event) {
+//             myMap.fitBounds(event.target.getBounds());
+//           }
+//         });
+//         // Giving each feature a pop-up with information pertinent to it
+//         layer.bindPopup("<h1>" + feature.properties.NAME10);
   
-      }
-    }).addTo(data_state);
-    // Then we add the earthquake layer to our map.
-   data_state.addTo(myMap);
+//       }
+//     }).addTo(data_state);
+//     // Then we add the earthquake layer to our map.
+//    data_state.addTo(myMap);
 
-// Then we add a control to the map that will allow the user to change which
-// layers are visible.
-L.control.layers(baseMaps, overlays).addTo(myMap);
+// // Then we add a control to the map that will allow the user to change which
+// // layers are visible.
+// L.control.layers(baseMaps, overlays).addTo(myMap);
 
 
 
-// ------ Major Earthquake  —————
-
-// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
-  
-
-  // 4. Use the same style as the earthquake data.
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
-  
-  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-  function getColor(magnitude) {
-    if (magnitude > 6) {
-      return "#ea2c2c";
-    }
-    if (magnitude > 5) {
-      return "#ea822c";
-    }
-    return "#eecc00";
-  }
-
-  
-  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
-  function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 4;
-  }
-  
-  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
-  // sets the style of the circle, and displays the magnitude and location of the earthquake
-  //  after the marker has been created and styled.
-  L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-        console.log(data);
-        return L.circleMarker(latlng);
-      },
-    // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo,
-   // We create a popup for each circleMarker to display the magnitude and location of the earthquake
-   //  after the marker has been created and styled.
-   onEachFeature: function(feature, layer) {
-    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-  }
-}).addTo(majorEarthquakes);
-
-  // 8. Add the major earthquakes layer to the map.
-  majorEarthquakes.addTo(map);
-  // 9. Close the braces and parentheses for the major earthquake data.
-  });
-
-// ------ Earthquake legend —————
-// Then add all the details for the legend
-legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-  
-    const d = [0, 1, 2, 3, 4, 5];
-    const colors = [
-      "#800026",
-      "#BD0026",
-      "#E31A1C",
-      "#FC4E2A",
-      "#FD8D3C",
-      "#FEB24C",
-      "FED976",
-      "FED976",
-  
-    ];
-  
-  
-  // Looping through our intervals to generate a label with a colored square for each interval.
-    for (var i = 0; i < d.length; i++) {
-      console.log(colors[i]);
-      div.innerHTML +=
-        "<i style='background: " + colors[i] + "'></i> " +
-        d[i] + (d[i + 1] ? "&ndash;" + d[i + 1] + "<br>" : "+");
-      }
-      return div;
-    };
-  
-    // Finally, we our legend to the map.
-    legend.addTo(myMap)
 
     });
 });
